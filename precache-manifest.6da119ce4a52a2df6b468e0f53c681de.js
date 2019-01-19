c767535ff1caf7d203edaa75a8bfd05ad1e12a8d
Adnan Babai <adnanbabai005@gmail.com>
2019-01-19 12:04:21 +0500

    react js

---
+++ precache-manifest.6da119ce4a52a2df6b468e0f53c681de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5db2f80d160dc328aec4",
+    "url": "/productShop/static/js/runtime~main.5db2f80d.js"
+  },
+  {
+    "revision": "b68bf2b854881e9a119a",
+    "url": "/productShop/static/js/main.b68bf2b8.chunk.js"
+  },
+  {
+    "revision": "121ac5c3180b8c8612ba",
+    "url": "/productShop/static/js/1.121ac5c3.chunk.js"
+  },
+  {
+    "revision": "b68bf2b854881e9a119a",
+    "url": "/productShop/static/css/main.7d241731.chunk.css"
+  },
+  {
+    "revision": "121ac5c3180b8c8612ba",
+    "url": "/productShop/static/css/1.6b36da78.chunk.css"
+  },
+  {
+    "revision": "202102989a57d2906fead6db15c71016",
+    "url": "/productShop/index.html"
+  }
+];
